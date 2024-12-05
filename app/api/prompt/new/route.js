affb80d8cefe5e58586b90acfaedc5f47b3c5c3d
Mahendra Jadav <137171724+Strange1108@users.noreply.github.com>
2024-12-05 15:33:31 +0530

    Update route.js

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -3,14 +3,16 @@ import { connectToDB } from "@utils/database";
 
 export const POST = async (request) => {
     const { userId, prompt, tag } = await request.json();
-
     try {
         await connectToDB();
+        console.log("create new prompt");
+        
         const newPrompt = new Prompt({ creator: userId, prompt, tag });
-
         await newPrompt.save();
-        return new Response(JSON.stringify(newPrompt), { status: 201 })
+        
+        return new Response(JSON.stringify(newPrompt), { status: 201 });
     } catch (error) {
+        console.error("Error creating prompt:", error); // Log the error for debugging
         return new Response("Failed to create a new prompt", { status: 500 });
     }
-}
+};
